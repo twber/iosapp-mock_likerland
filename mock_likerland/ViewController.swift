fae7365d5a0439fca8bb7575cf815f0bee866eca
twber <hctsai@umich.edu>
2020-07-22 19:52:09 +0800

    Initial Commit

---
+++ mock_likerland/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mock_likerland
+//
+//  Created by Bernice TSAI on 2020/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
